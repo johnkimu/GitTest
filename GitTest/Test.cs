775c68532be2a26fdde210a480df29af1e587bcd
Petur Thorgeirsson <petu0125@edu.easj.dk>
2015-11-04 11:45:54 +0100

    sdf

--- GitTest/Test.cs
+++ GitTest/Test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
 
 namespace GitTest
 {
@@ -13,5 +14,7 @@ namespace GitTest
         {
             age = age_in;
         }
+
+        
     }
 }
